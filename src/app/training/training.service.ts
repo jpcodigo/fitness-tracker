f9e2a024574890130921ddb67e962946bb264814
jpcodigo <68927440+jpcodigo@users.noreply.github.com>
2020-10-25 19:35:16 -0400

    added training service

---
+++ src/app/training/training.service.ts
@@ -0,0 +1,23 @@
+import { Subject } from 'rxjs';
+import { Exercise } from './exercise.model';
+
+
+export class TrainingService {
+    exerciseChanged = new Subject<Exercise>();
+    private availableExercises: Exercise[] = [
+        { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
+        { id: 'toe-touches', name: 'Toe-touches', duration: 180, calories: 15 },
+        { id: 'lunges', name: 'Lunges', duration: 120, calories: 18 },
+        { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 }
+    ];
+    private runningExercise: Exercise;
+
+    getAvailableExercises() {
+        return this.availableExercises.slice();
+    }
+
+    startExercise(selectedId: string) {
+        this.runningExercise = this.availableExercises.find(ex => ex.id === selectedId);
+        this.exerciseChanged.next({ ...this.runningExercise });
+    }
+}
