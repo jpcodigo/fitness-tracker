56816018701f3f6bcf733c05e497529b0ec07d53
jpcodigo <68927440+jpcodigo@users.noreply.github.com>
2020-10-26 03:39:41 -0400

    added sorting and filter

--- src/app/training/past-training-sessions/past-training-sessions.component.ts
+++ src/app/training/past-training-sessions/past-training-sessions.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Exercise } from '../exercise.model';
+import { TrainingService } from '../training.service';
 
 @Component({
   selector: 'app-past-training-sessions',
   templateUrl: './past-training-sessions.component.html',
   styleUrls: ['./past-training-sessions.component.css']
 })
-export class PastTrainingSessionsComponent implements OnInit {
+export class PastTrainingSessionsComponent implements OnInit, AfterViewInit {
+  displayedColumns = ['date', 'name', 'calories', 'duration', 'state'];
+  dataSource = new MatTableDataSource<Exercise>();
 
-  constructor() { }
+  @ViewChild(MatSort, { static: false }) sort: MatSort;
+
+  constructor(private trainingService: TrainingService) { }
 
   ngOnInit(): void {
+    this.dataSource.data = this.trainingService.getCompletedOrCancelledExercises();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+  doFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
